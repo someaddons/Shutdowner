05f6030a6511003d60cda42cc9a490a76dc3e90d
someaddons <someaddons@github.com>
2026-01-30 11:42:52 +0100

    2.4: Fix crashing in singleplayer

--- src/main/java/com/shutdowner/event/EventHandler.java
+++ src/main/java/com/shutdowner/event/EventHandler.java
@@ -47,7 +47,6 @@ public class EventHandler
     }
 
     @SubscribeEvent
-    @OnlyIn(Dist.DEDICATED_SERVER)
     public static void onServerTick(final ServerTickEvent.Post event)
     {
         watcherThread.onServerTick();
