0f0b2d1cb33678b9ced5860efa4c9d62ba9e0057
someaddons <someaddons@github.com>
2026-01-30 11:57:55 +0100

    2.4: Fix crashing in singleplayer

--- src/main/java/com/shutdowner/event/EventHandler.java
+++ src/main/java/com/shutdowner/event/EventHandler.java
@@ -3,14 +3,13 @@ package com.shutdowner.event;
 import com.shutdowner.Shutdowner;
 import com.shutdowner.handlers.ShutDownHandler;
 import com.shutdowner.threading.WatcherThread;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.event.level.LevelEvent;
 import net.minecraftforge.event.server.ServerStartedEvent;
 import net.minecraftforge.event.server.ServerStoppedEvent;
 import net.minecraftforge.event.server.ServerStoppingEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.loading.FMLEnvironment;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -26,21 +25,18 @@ public class EventHandler
     private static WatcherThread watcherThread;
 
     @SubscribeEvent
-    @OnlyIn(Dist.DEDICATED_SERVER)
     public static void onServerStopping(final ServerStoppingEvent event)
     {
         watcherThread.notifyShutDownEvent();
     }
 
     @SubscribeEvent
-    @OnlyIn(Dist.DEDICATED_SERVER)
     public static void onServerStopping(final ServerStoppedEvent event)
     {
         watcherThread.notifyShutDownDone();
     }
 
     @SubscribeEvent
-    @OnlyIn(Dist.DEDICATED_SERVER)
     public static void onServerStarted(final ServerStartedEvent event)
     {
         ShutDownHandler.onServerStart();
@@ -50,13 +46,15 @@ public class EventHandler
     public static void onServerTick(final TickEvent.ServerTickEvent event)
     {
         watcherThread.onServerTick();
-        ShutDownHandler.onServerTick();
+        if (FMLEnvironment.dist.isDedicatedServer())
+        {
+            ShutDownHandler.onServerTick();
+        }
     }
 
     public static ExecutorService executor = Executors.newFixedThreadPool(1);
 
     @SubscribeEvent
-    @OnlyIn(Dist.DEDICATED_SERVER)
     public static void onWorldLoad(final LevelEvent.Load event)
     {
         if (watcherThread == null && Shutdowner.getConfig().getCommonConfig().shouldDetectShutDownHang.get())
